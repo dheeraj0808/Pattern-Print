82e62b61b65d539c5c8243fad2417e7d777de020
dheeraj0808 <dheerajsingh1939@gmail.com>
2025-07-21 13:14:50 +0530

    inverted triangle added

--- pattern.js
+++ pattern.js
@@ -1,7 +1,17 @@
-// basic code
-let rows = 5;
 
+let rows = 5;
+// right angle triangle
 for (let i = 1; i <= rows; i++) {
+    let star = '';
+    for (let j = 1; j <= i; j++) {
+        star += '* ';
+    }
+    console.log(star);
+}
+// Inverted triangle pattern
+
+
+for (let i = rows; i >= 1; i--) {
     let star = '';
     for (let j = 1; j <= i; j++) {
         star += '* ';
