bea16b9cc92dc1f79e142abf807e684ce16904fe
dheeraj0808 <dheerajsingh1939@gmail.com>
2025-07-23 00:46:33 +0530

    rectangle and square

--- pattern.js
+++ pattern.js
@@ -9,9 +9,8 @@ for (let i = 1; i <= rows; i++) {
     console.log(star);
 }
 console.log(" ");
+
 // 2.Inverted triangle pattern
-
-
 for (let i = rows; i >= 1; i--) {
     let star = '';
     for (let j = 1; j <= i; j++) {
@@ -20,13 +19,32 @@ for (let i = rows; i >= 1; i--) {
     console.log(star);
 }
 console.log(" ");
+
 // 3.Right alligne triangle
 for (let i = 1; i <= rows; i++) {
     let star = " ";
     for (let j = 1; j <= rows - i; j++) star += " ";
-    for (let k = 1; k <= i; k++) star += "*";
+    for (let k = 1; k <= i; k++) star += "* ";
     console.log(star);
 }
 console.log(" ");
-//4. 
+
+//4. square code
+for (let i = 1; i <= rows; i++) {
+    let star = " ";
+    for (let j = 1; j <= rows; j++) {
+        star += '* '
+    }
+    console.log(star);
+}
+console.log(" ");
+
+//5. rectangle code 
+for (let i = 1; i <= 4; i++) {
+    let star = "";
+    for (let j = 1; j <= 6; j++) {
+        star += ' *';
+    }
+    console.log(star);
+}
 
