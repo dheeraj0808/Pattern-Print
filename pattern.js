1188e18c513d1a2993f94de851f3fb09aa332a7d
dheeraj0808 <dheerajsingh1939@gmail.com>
2025-11-16 23:30:25 +0530

    everse of right angled triangle with increaseing number in row

--- pattern.js
+++ pattern.js
@@ -58,5 +58,13 @@ for (let i = 1; i <= 4; i++) {
 }
 console.log(" ");
 
-// 7.
+// 7. reverse of right angled triangle with increaseing number in row
+for (let i = 0; i <= 5; i++) {
+    let row = ""
+    for (let j = 0; j <= n - i; j++) {
+        row = row + (j + 1);
+    }
+    console.log(row)
+}
+
 
